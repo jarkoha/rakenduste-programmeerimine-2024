a6a946ece37ab5b03c2295245727b4e49e194dbd
Jarl Koha <146342562+jarkoha@users.noreply.github.com>
2024-09-15 18:16:20 +0300

    feat: Counter button values

--- 2024-09-09/round-turtle/src/components/Counter.js
+++ 2024-09-09/round-turtle/src/components/Counter.js
@@ -4,17 +4,19 @@ const Counter = () => {
   const [counter, setCounter] = useState(0)
   // const [counter, setCounter] = React.useState(0);
 
-  const modifyCounter = () => setCounter(prevCounter => prevCounter + 1)
+  const modifyCounter = (amount) => setCounter(prevCounter => prevCounter + amount)
 
   return (
     <>
       <h1>{counter}</h1>
 
       {[+1, +5, +50, -1, -5, -50].map(element => (
-        <button onClick={modifyCounter}>sync {element}</button>
+        <button key={element} onClick={() => modifyCounter(element)}> 
+          {element > 0 ? `+${element}` : element}
+        </button>
       ))}
 
-      <button onClick={() => setTimeout(() => modifyCounter(), 2000)}>
+      <button onClick={() => setTimeout(() => modifyCounter(1), 2000)}>
         async +1
       </button>
     </>
